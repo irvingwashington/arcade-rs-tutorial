d630e20db5d8ad0210f438fec341ab127804f3f4
Maciek Dubinski <maciek@dubinski.net>
2017-01-02 23:01:05 +0100

    Bullets

--- src/phi/gfx.rs
+++ src/phi/gfx.rs
@@ -52,17 +52,82 @@ impl Sprite {
         (self.src.w, self.src.h)
     }
 
-    pub fn render(&self, renderer: &mut Renderer, dest: Rectangle) {
+}
+
+impl Renderable for Sprite {
+    fn render(&self, renderer: &mut Renderer, dest: Rectangle) {
         renderer.copy(&mut self.tex.borrow_mut(), self.src.to_sdl(), dest.to_sdl());
     }
 }
 
-pub trait CopySprite {
-    fn copy_sprite(&mut self, sprite: &Sprite, dest: Rectangle);
+pub trait CopySprite<T> {
+    fn copy_sprite(&mut self, renderable: &T, dest: Rectangle);
 }
 
-impl<'window> CopySprite for Renderer<'window> {
-    fn copy_sprite(&mut self, sprite: &Sprite, dest: Rectangle) {
-        sprite.render(self, dest);
+impl<'window, T: Renderable> CopySprite<T> for Renderer<'window> {
+    fn copy_sprite(&mut self, renderable: &T, dest: Rectangle) {
+        renderable.render(self, dest);
+    }
+}
+
+#[derive(Clone)]
+pub struct AnimatedSprite {
+    sprites:  Rc<Vec<Sprite>>, //Vec<Sprite>,
+    frame_delay: f64,
+    current_time: f64,
+}
+
+impl AnimatedSprite {
+
+    pub fn new(sprites: Vec<Sprite>, frame_delay: f64) -> AnimatedSprite {
+        AnimatedSprite {
+            sprites: Rc::new(sprites),
+            frame_delay: frame_delay,
+            current_time: 0.0
+        }
+    }
+
+    pub fn with_fps(sprites: Vec<Sprite>, fps: f64) -> AnimatedSprite {
+        if fps == 0.0 {
+            panic!("Passed 0 to AnimatedSprite::with_fps");
+        }
+        AnimatedSprite::new(sprites, 1.0 / fps)
+    }
+
+    pub fn frames(&self) -> usize {
+        self.sprites.len()
+    }
+
+    pub fn set_frame_delay(&mut self, frame_delay: f64) {
+        self.frame_delay = frame_delay;
+    }
+
+    pub fn set_fps(&mut self, fps: f64) {
+        if fps == 0.0 {
+            panic!("Passed 0 to AnimatedSprite::set_fps");
+        }
+
+        self.set_frame_delay(1.0 / fps);
+    }
+
+    pub fn add_time(&mut self, dt: f64) {
+        self.current_time += dt;
+
+        if self.current_time < 0.0 {
+            self.current_time = (self.frames() - 1) as f64 * self.frame_delay;
+        }
+    }
+}
+
+pub trait Renderable {
+    fn render(&self, renderer: &mut Renderer, dest: Rectangle);
+}
+
+impl Renderable for AnimatedSprite {
+    fn render(&self, renderer: &mut Renderer, dest: Rectangle) {
+        let current_frame = (self.current_time / self.frame_delay) as usize % self.frames();
+
+        let sprite = &self.sprites[current_frame];
+        sprite.render(renderer, dest);
     }
 }
--- src/views/game.rs
+++ src/views/game.rs
@@ -6,13 +6,24 @@ use sdl2::render::Renderer;
 
 use phi::{Phi, View, ViewAction};
 use phi::data::Rectangle;
-use phi::gfx::{CopySprite, Sprite};
+use phi::gfx::{CopySprite, Sprite, AnimatedSprite};
 use views::shared::Background;
 
 const PLAYER_SPEED: f64 = 180.0;
 const SHIP_H: f64 = 39.0;
 const SHIP_W: f64 = 43.0;
 const DEBUG: bool = false;
+
+const BULLET_SPEED: f64 = 240.0;
+
+const ASTEROID_PATH: &'static str = "assets/asteroid.png";
+const ASTEROIDS_WIDE: usize = 21;
+const ASTEROIDS_HIGH: usize = 7;
+const ASTEROIDS_TOTAL: usize = ASTEROIDS_WIDE * ASTEROIDS_HIGH - 4;
+const ASTEROID_SIDE: f64 = 96.0;
+
+const BULLET_W: f64 = 8.0;
+const BULLET_H: f64 = 4.0;
 
 #[derive(Clone, Copy)]
 enum ShipFrame {
@@ -31,11 +42,61 @@ struct Ship {
     rect: Rectangle,
     sprites: Vec<Sprite>,
     current: ShipFrame,
+    cannon: CannonType,
+}
+
+impl Ship {
+    fn spawn_bullets(&self) -> Vec<Box<Bullet>> {
+        let cannons_x = self.rect.x + 30.0;
+        let cannon1_y = self.rect.y + 6.0;
+        let cannon2_y = self.rect.y + SHIP_H - 10.0;
+
+
+        match self.cannon {
+            CannonType::RectBullet =>
+                vec![
+                    Box::new(RectBullet {
+                        rect: Rectangle {
+                            x: cannons_x,
+                            y: cannon1_y,
+                            w: BULLET_W,
+                            h: BULLET_H,
+                        }
+                    }),
+                    Box::new(RectBullet {
+                        rect: Rectangle {
+                            x: cannons_x,
+                            y: cannon2_y,
+                            w: BULLET_W,
+                            h: BULLET_H,
+                        }
+                    }),
+                ],
+             CannonType::SineBullet { amplitude, angular_vel } =>
+                vec![
+                    Box::new(SineBullet {
+                        pos_x: cannons_x,
+                        origin_y: cannon1_y,
+                        amplitude: amplitude,
+                        angular_vel: angular_vel,
+                        total_time: 0.0,
+                    }),
+                    Box::new(SineBullet {
+                        pos_x: cannons_x,
+                        origin_y: cannon2_y,
+                        amplitude: amplitude,
+                        angular_vel: angular_vel,
+                        total_time: 0.0,
+                    }),
+                ]
+        }
+    }
 }
 
 pub struct ShipView {
     player: Ship,
-
+    asteroid: Asteroid,
+    bullets: Vec<Box<Bullet>>,
     bg_back: Background,
     bg_middle: Background,
     bg_front: Background,
@@ -58,6 +119,7 @@ impl ShipView {
         }
 
         ShipView {
+            asteroid: Asteroid::new(phi),
             player: Ship {
                 rect: Rectangle {
                     x: 64.0,
@@ -67,8 +129,9 @@ impl ShipView {
                 },
                 sprites: sprites,
                 current: ShipFrame::MidNorm,
+                cannon: CannonType::RectBullet,
             },
-
+            bullets: vec![],
             bg_back: Background {
                 pos: 0.0,
                 vel: 20.0,
@@ -92,6 +155,20 @@ impl View for ShipView {
     fn render(&mut self, phi: &mut Phi, elapsed: f64) -> ViewAction {
         if phi.events.now.quit || phi.events.now.key_escape == Some(true) {
             return ViewAction::Quit;
+        }
+
+        if phi.events.now.key_1 == Some(true) {
+            self.player.cannon = CannonType::RectBullet;
+        }
+
+        if phi.events.now.key_2 == Some(true) {
+            self.player.cannon = CannonType::SineBullet {
+                amplitude: 10.0,
+                angular_vel: 15.0,
+            };
+        }
+
+        if phi.events.now.key_3 == Some(true) {
         }
 
         phi.renderer.set_draw_color(Color::RGB(0, 0, 0));
@@ -145,15 +222,181 @@ impl View for ShipView {
             else if dx < 0.0 && dy > 0.0   { ShipFrame::DownSlow }
             else { unreachable!() };
 
+        let old_bullets = ::std::mem::replace(&mut self.bullets, vec![]);
+        self.bullets = old_bullets.into_iter().filter_map(|bullet| bullet.update(phi, elapsed)).collect();
+
         self.player.rect = self.player.rect.move_inside(movable_region).unwrap();
+
+        if phi.events.now.key_space == Some(true) {
+            self.bullets.append(&mut self.player.spawn_bullets());
+        }
+
+        self.asteroid.update(phi, elapsed);
 
         phi.renderer.set_draw_color(Color::RGB(200, 200, 50));
         phi.renderer.copy_sprite(&self.player.sprites[self.player.current as usize], self.player.rect);
 
+        for bullet in &self.bullets {
+            bullet.render(phi);
+        }
+        self.asteroid.render(phi);
         self.bg_front.render(&mut phi.renderer, elapsed);
 
         ViewAction::None
    }
 }
 
+struct Asteroid {
+    sprite: AnimatedSprite,
+    rect: Rectangle,
+    vel: f64,
+}
 
+impl Asteroid {
+    fn get_sprite(phi: &mut Phi, fps: f64) -> AnimatedSprite {
+        let asteroid_spritesheet = Sprite::load(&mut phi.renderer, ASTEROID_PATH).unwrap();
+        let mut asteroid_sprites = Vec::with_capacity(ASTEROIDS_TOTAL);
+
+        for yth in 0..ASTEROIDS_HIGH {
+            for xth in 0..ASTEROIDS_WIDE {
+                //? There are four asteroids missing at the end of the
+                //? spritesheet: we do not want to render those.
+                if ASTEROIDS_WIDE * yth + xth >= ASTEROIDS_TOTAL {
+                    break;
+                }
+
+                asteroid_sprites.push(
+                    asteroid_spritesheet.region(Rectangle {
+                        w: ASTEROID_SIDE,
+                        h: ASTEROID_SIDE,
+                        x: ASTEROID_SIDE * xth as f64,
+                        y: ASTEROID_SIDE * yth as f64,
+                    }).unwrap());
+            }
+        }
+
+        AnimatedSprite::with_fps(asteroid_sprites, fps)
+    }
+
+    fn new(phi: &mut Phi) -> Asteroid {
+        let mut asteroid = Asteroid {
+            sprite: Asteroid::get_sprite(phi, 15.0),
+            rect: Rectangle {
+                w: ASTEROID_SIDE,
+                h: ASTEROID_SIDE,
+                x: 128.0,
+                y: 128.0,
+            },
+            vel: 0.0,
+        };
+        asteroid.reset(phi);
+        asteroid
+    }
+
+    fn reset(&mut self, phi: &mut Phi) {
+        let (w, h) = phi.output_size();
+
+        self.sprite.set_fps(::rand::random::<f64>().abs() * 20.0 + 10.0);
+
+        self.rect = Rectangle {
+            w: ASTEROID_SIDE,
+            h: ASTEROID_SIDE,
+            x: w,
+            y: ::rand::random::<f64>().abs() * (h - ASTEROID_SIDE),
+        };
+
+        self.vel = ::rand::random::<f64>().abs() * 100.0 + 50.0;
+    }
+
+    fn update(&mut self, phi: &mut Phi, dt: f64) {
+        self.rect.x -= dt * self.vel;
+        self.sprite.add_time(dt);
+
+        if self.rect.x <= -ASTEROID_SIDE {
+            self.reset(phi);
+        }
+    }
+
+    fn render(&mut self, phi: &mut Phi) {
+        phi.renderer.copy_sprite(&self.sprite, self.rect);
+    }
+}
+
+trait Bullet {
+    fn update(self: Box<Self>, phi: &mut Phi, dt: f64) -> Option<Box<Bullet>>;
+    fn render(&self, phi: &mut Phi);
+    fn rect(&self) -> Rectangle;
+}
+
+#[derive(Clone, Copy)]
+struct RectBullet {
+    rect: Rectangle,
+}
+
+impl Bullet for RectBullet {
+    fn update(mut self: Box<Self>, phi: &mut Phi, dt: f64) -> Option<Box<Bullet>> {
+        let (w, _) = phi.output_size();
+        self.rect.x += BULLET_SPEED * dt;
+
+        if self.rect.x > w {
+            None
+        } else {
+            Some(self)
+        }
+    }
+
+    fn render(&self, phi: &mut Phi) {
+        phi.renderer.set_draw_color(Color::RGB(230, 230, 30));
+        phi.renderer.fill_rect(self.rect.to_sdl().unwrap());
+    }
+
+    fn rect(&self) -> Rectangle {
+        self.rect
+    }
+}
+
+struct SineBullet {
+    pos_x: f64,
+    origin_y: f64,
+    amplitude: f64,
+    angular_vel: f64,
+    total_time: f64,
+}
+
+impl Bullet for SineBullet {
+    fn update(mut self: Box<Self>, phi: &mut Phi, dt: f64) -> Option<Box<Bullet>> {
+        self.total_time += dt;
+
+        self.pos_x += BULLET_SPEED * dt;
+
+        let (w, _) = phi.output_size();
+
+        if self.rect().x > w {
+            None
+        } else {
+            Some(self)
+        }
+    }
+
+    fn render(&self, phi: &mut Phi) {
+        phi.renderer.set_draw_color(Color::RGB(230, 230, 30));
+        phi.renderer.fill_rect(self.rect().to_sdl().unwrap());
+    }
+
+    fn rect(&self) -> Rectangle {
+        let dy = self.amplitude * f64::sin(self.angular_vel * self.total_time);
+
+        Rectangle {
+            x: self.pos_x,
+            y: self.origin_y + dy,
+            w: BULLET_W,
+            h: BULLET_H,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum CannonType {
+    RectBullet,
+    SineBullet { amplitude: f64, angular_vel: f64 },
+}
